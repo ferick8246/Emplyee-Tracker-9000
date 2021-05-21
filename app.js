84cf070c825663e0055a5515b17920029be71553
Erick Flore <ferick8246@gmail.com>
2021-05-21 09:54:11 -0500

    async function

--- app.js
+++ app.js
@@ -3,3 +3,28 @@ const mysql = require("mysql")
 const connection = require("./assets/js/connection");
 const inquirer = require("inquirer");
 const questions = require("./assets/js/questions");
+
+init();
+
+async function init() {
+  const { action } = await inquirer.prompt(questions);
+  switch (action) {
+    case "Edit Department":
+     editDepartments();
+      break;
+    case "Edit Employee Role":
+      editRole();
+      break;
+    case "Edit Employee":
+      editEmployee();
+      break;
+    case "View Information":
+      viewInfo();
+      break;
+    case "Exit":
+      process.exit(0);
+      break;
+    default:
+      break;
+  }
+}
