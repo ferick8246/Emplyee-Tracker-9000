c317bb15fae100a154f297ab876a1eaa1b16211d
Erick Flore <ferick8246@gmail.com>
2021-05-21 20:18:54 -0500

    department table

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+-- Table for the departments 
+INSERT INTO department (dept) VALUES ('leagal');
+INSERT INTO department (dept) VALUES ('engineering');
+INSERT INTO department (dept) VALUES ('finance');
+INSERT INTO department (dept) VALUES ('sales');
+
