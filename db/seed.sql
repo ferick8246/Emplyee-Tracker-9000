3302ff3d73089f4305db4e6ce1e67cd73ca9dd56
Erick Flore <ferick8246@gmail.com>
2021-05-21 20:21:22 -0500

    role table

--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,8 @@ INSERT INTO department (dept) VALUES ('engineering');
 INSERT INTO department (dept) VALUES ('finance');
 INSERT INTO department (dept) VALUES ('sales');
 
+-- Table for Roles 
+INSERT INTO role (title, salary, dept_id) VALUES ('Accountant', 60000, 1);
+INSERT INTO role (title, salary, dept_id)  VALUES ('Intern', 50000, 4);
+INSERT INTO role (title, salary, dept_id)  VALUES ('Engineer', 90000, 2);
+INSERT INTO role (title, salary, dept_id)  VALUES ('IT', 80000, 3);
