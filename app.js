d1fb5f5af07d3804709ff03392a5cc1e432a686c
Erick Flore <ferick8246@gmail.com>
2021-05-21 21:22:07 -0500

    view function added

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const mysql = require("mysql")
-
-const connection = require("./assets/js/connection");
+const connection = require("/assets/connection.js");
 const inquirer = require("inquirer");
-const questions = require("./assets/js/questions");
+const questions = require("/assets/question.js");
 
 init();
 
@@ -28,3 +26,29 @@ const { action } = await inquirer.prompt(questions);
       break;
   }
 }
+// View 
+async function viewInfo() {
+    const { letsGo } = await inquirer.prompt({
+      name: 'letsGo',
+      type: 'list',
+      message: 'Select a topic',
+      choices: ['Employees', 'Departments', 'Roles']
+    });
+    let query;
+    if (letsGo === 'Employees') {
+      query = `SELECT  employee.first_name, employee.last_name,
+        role.title, role.salary, department.dept AS department
+        FROM ((employee
+        INNER JOIN role ON employee.role_id = role.id)
+        INNER JOIN department ON role.dept_id = department.id)
+        ORDER BY department`;
+    } else if (letsGo === 'Departments') {
+      query = `SELECT dept FROM department`;
+    } else if (letsGo === 'Roles') {
+      query = `SELECT role.title, role.dept_id AS id, department.dept AS department FROM role 
+      INNER JOIN department ON role.dept_id = department.id ORDER BY title ASC`;
+    }
+    const data = await connection.query(query);
+    console.table(data);
+    init();
+  }
