df53268fa47976224028de69961ed2bd52ab8c4a
Erick Flore <ferick8246@gmail.com>
2021-05-21 23:47:38 -0500

    deparment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const connection = require("/assets/connection.js");
+const connection = require("./assets/connection.js");
 const inquirer = require("inquirer");
-const questions = require("/assets/question.js");
+const questions = require("./assets/question.js");
 
 init();
 
@@ -26,29 +26,25 @@ const { action } = await inquirer.prompt(questions);
       break;
   }
 }
-// View 
-async function viewInfo() {
-    const { letsGo } = await inquirer.prompt({
-      name: 'letsGo',
-      type: 'list',
-      message: 'Select a topic',
-      choices: ['Employees', 'Departments', 'Roles']
-    });
-    let query;
-    if (letsGo === 'Employees') {
-      query = `SELECT  employee.first_name, employee.last_name,
-        role.title, role.salary, department.dept AS department
-        FROM ((employee
-        INNER JOIN role ON employee.role_id = role.id)
-        INNER JOIN department ON role.dept_id = department.id)
-        ORDER BY department`;
-    } else if (letsGo === 'Departments') {
-      query = `SELECT dept FROM department`;
-    } else if (letsGo === 'Roles') {
-      query = `SELECT role.title, role.dept_id AS id, department.dept AS department FROM role 
-      INNER JOIN department ON role.dept_id = department.id ORDER BY title ASC`;
-    }
-    const data = await connection.query(query);
-    console.table(data);
+// Edit Department
+async function editDepartments() {
+  const { department } = await inquirer.prompt({
+    name: "department",
+    type: "list",
+    message: "Choose one of the following:",
+    choices: [
+      "Add Department",
+      "Remove Department",
+      "Exit"
+    ]
+  })
+  if (department === "Add Department") {
+    addDepartment();
+  }
+  if (department === "Remove Department") {
+    remDepartment();
+  }
+  if (department === "Exit") {
     init();
   }
+};
