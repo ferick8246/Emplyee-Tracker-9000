5a8085b024fd691350ba29f89c14b0c12336d2f0
Erick Flore <ferick8246@gmail.com>
2021-05-22 00:43:29 -0500

    View function

--- app.js
+++ app.js
@@ -267,3 +267,30 @@ async function updateRole() {
       init();
     });
 };
+// View function
+
+async function viewInfo() {
+  const { letsGo } = await inquirer.prompt({
+    name: 'letsGo',
+    type: 'list',
+    message: 'Select a topic',
+    choices: ['Employees', 'Departments', 'Roles']
+  });
+  let query;
+  if (letsGo === 'Employees') {
+    query = `SELECT  employee.first_name, employee.last_name,
+      role.title, role.salary, department.dept AS department
+      FROM ((employee
+      INNER JOIN role ON employee.role_id = role.id)
+      INNER JOIN department ON role.dept_id = department.id)
+      ORDER BY department`;
+  } else if (letsGo === 'Departments') {
+    query = `SELECT dept FROM department`;
+  } else if (letsGo === 'Roles') {
+    query = `SELECT role.title, role.dept_id AS id, department.dept AS department FROM role 
+    INNER JOIN department ON role.dept_id = department.id ORDER BY title ASC`;
+  }
+  const data = await connection.query(query);
+  console.table(data);
+  init();
+}
