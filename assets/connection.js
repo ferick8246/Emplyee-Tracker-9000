1ae5b9bae8ffdf5bc3aae828e02e8578de97e6bc
Erick Flore <ferick8246@gmail.com>
2021-05-21 20:07:22 -0500

    created connection to mySQL

---
+++ assets/connection.js
@@ -0,0 +1,16 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "2696",
+  database: "employee_DB",
+});
+
+connection.connect();
+
+// Setting up connection.query for promises instead of callbacks allowing to useasync/await syntax
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
